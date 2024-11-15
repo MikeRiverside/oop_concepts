5e7833bfc19152458968b3002030f9f6e1ac315e
Mihkel Jõesalu <mihkel.joesalu@voco.ee>
2024-11-15 16:53:43 +0200

    polümorfism

---
+++ poly.js
@@ -0,0 +1,26 @@
+class Circle {
+    corners() {
+        console.log("Circle doesn't have any corners")
+    }
+
+    sides() {
+        console.log("Circle has one side")
+    }
+}
+class Rectangle {
+    corners() {
+        console.log("Rectangle has 4 corners")
+    }
+
+    sides() {
+        console.log("Rectangle has 4 sides")
+    }
+}
+const cornersTest = (shape) => {
+    shape.corners()
+}
+const shape1 = new Circle()
+const shape2 = new Rectangle()
+
+cornersTest(shape1)
+cornersTest(shape2)
